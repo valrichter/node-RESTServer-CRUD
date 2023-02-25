3039c7af3eb34a764f129cc757daa2b6a3d0f440
valrichter <valenrichter11@gmail.com>
2023-02-25 18:59:33 -0300

    add JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const Usuario = require("../models/usuario");
 const bcryptjs = require("bcryptjs");
+const { generarJWT } = require("../helpers/generar-jwt");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -30,9 +31,11 @@ const login = async (req, res = response) => {
     }
 
     // Generar el JWT
+    const token = await generarJWT(usuario.id);
 
     res.json({
-      msj: "Login OK",
+      usuario,
+      token,
     });
   } catch (error) {
     console.log(error);
