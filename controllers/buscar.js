19778a6d4ee90d9a538467e487a634160eddf15d
valrichter <valenrichter11@gmail.com>
2023-03-04 21:12:54 -0300

    add busqueda de productos y categorias

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -24,6 +24,48 @@ const buscarUsuarios = async (termino = "", res = response) => {
   });
 };
 
+const buscarCategorias = async (termino = "", res = response) => {
+  const esMongoID = ObjectId.isValid(termino);
+
+  if (esMongoID) {
+    const categoria = await Categoria.findById(termino);
+    return res.json({
+      results: categoria ? [categoria] : [],
+    });
+  }
+
+  const regex = new RegExp(termino, "i");
+
+  const categorias = await Categoria.find({ nombre: regex, estado: true });
+  return res.json({
+    results: categorias,
+  });
+};
+
+const buscarProductos = async (termino = "", res = response) => {
+  const esMongoID = ObjectId.isValid(termino);
+
+  if (esMongoID) {
+    const producto = await Producto.findById(termino).populate(
+      "categoria",
+      "nombre"
+    );
+    return res.json({
+      results: producto ? [producto] : [],
+    });
+  }
+
+  const regex = new RegExp(termino, "i");
+
+  const productos = await Producto.find({
+    nombre: regex,
+    estado: true,
+  }).populate("categoria", "nombre");
+  return res.json({
+    results: productos,
+  });
+};
+
 const buscar = (req = request, res = response) => {
   const { coleccion, termino } = req.params;
 
@@ -39,9 +81,11 @@ const buscar = (req = request, res = response) => {
       break;
 
     case "categorias":
+      buscarCategorias(termino, res);
       break;
 
     case "productos":
+      buscarProductos(termino, res);
       break;
 
     default:
